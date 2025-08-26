f0230ee06dcb9740b6bcfc9cae1cafc764701dc8
infintelyPained <crimsondusk2025@outlook.com>
2025-08-25 22:05:36 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// server.js - WebSocket signaling server for Netplay
+// Simple WebSocket signaling server for EmulatorJS Netplay
 const http = require('http');
 const WebSocket = require('ws');
 
@@ -7,7 +7,6 @@ const wss = new WebSocket.Server({ server });
 
 wss.on('connection', (ws) => {
   ws.on('message', (message) => {
-    // Broadcast received message to all clients except sender
     wss.clients.forEach((client) => {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(message);
